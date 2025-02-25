6c2905eb57a0f2cfe0a0668f437f08483075affc
BobbyLin23 <linzhangsheng23@gmail.com>
2025-02-25 21:32:51 +0800

    feat: add ui and eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  css: ['~/assets/css/main.css'],
+  devServer: {
+    port: 3019,
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui',
+  },
 })
